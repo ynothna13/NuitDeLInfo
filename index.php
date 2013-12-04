6486ba7785c29c54036988a9aa6c31226c94034a
ynothna13 <dacruzanthony@gmail.com>
2013-12-04 16:27:50 +0100

    test index.php

--- index.php
+++ index.php
@@ -1,30 +1,4 @@
 <?php
-echo '
-	<!doctype html>
-	<html lang="fr">
-		<head>
-			<meta charset="utf-8">
-			<!-- <meta name="viewport" content="width=device-width" /> -->
-			<link rel="icon" href="images/favicon.png" type="image/ico" /> <!--Icone de la page (favicon) Firefox-->
-			<link rel="shortcut icon" type="image/x-icon" href="images/favicon.png" /> <!--Icone de la page (favicon) IE-->
-
-
-			<title>Appli WEB Nuit De L\'Info</title>
-			
-			<link rel="stylesheet" type="text/css" href="styles.css"/>
-			<script language="javascript" type="text/javascript" src="jquery.js"></script>
-			<script language="javascript" type="text/javascript" src="jquery.easing.1.3.js"></script>
-			<script language="javascript" type="text/javascript" src="script.js"></script>
-			
-		</head>
-		<body>
-			<center><h1>Voici la BASE</h1></center>
-		</body>
-';
-
-
-
-echo 'salut';
-
+echo 'flo calme toi';
 
 ?>
