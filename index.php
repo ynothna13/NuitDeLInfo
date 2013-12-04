02e511b27f42001adfc1e0ede1d94ffa89a3ed94
ynothna13 <dacruzanthony@gmail.com>
2013-12-04 15:40:50 +0100

    Base HTML

--- index.php
+++ index.php
@@ -1,4 +1,25 @@
 <?php
-echo "<center><h1>Voici la Base de zizi</h1></center>";
-echo 'coucou';
+echo '
+	<!doctype html>
+	<html lang="fr">
+		<head>
+			<meta charset="utf-8">
+			<!-- <meta name="viewport" content="width=device-width" /> -->
+			<link rel="icon" href="images/favicon.png" type="image/ico" /> <!--Icone de la page (favicon) Firefox-->
+			<link rel="shortcut icon" type="image/x-icon" href="images/favicon.png" /> <!--Icone de la page (favicon) IE-->
+
+			<title>Appli WEB Nuit De L\'Info</title>
+			
+			<link rel="stylesheet" type="text/css" href="styles.css"/>
+			<script language="javascript" type="text/javascript" src="jquery.js"></script>
+			<script language="javascript" type="text/javascript" src="jquery.easing.1.3.js"></script>
+			<script language="javascript" type="text/javascript" src="script.js"></script>
+			
+		</head>
+		<body>
+			<center><h1>Voici la BASE</h1></center>
+		</body>
+';
+
+
 ?>
