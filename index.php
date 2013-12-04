26bd4351282add396651b43095a5dab3e560b556
ynothna13 <dacruzanthony@gmail.com>
2013-12-04 15:47:54 +0100

    !

--- index.php
+++ index.php
@@ -1,39 +1,8 @@
 <?php
 
-echo '
-	<!doctype html>
-	<html lang="fr">
-		<head>
-			<meta charset="utf-8">
-			<!-- <meta name="viewport" content="width=device-width" /> -->
-			<link rel="icon" href="images/favicon.png" type="image/ico" /> <!--Icone de la page (favicon) Firefox-->
-			<link rel="shortcut icon" type="image/x-icon" href="images/favicon.png" /> <!--Icone de la page (favicon) IE-->
-
-			<title>Appli WEB Nuit De L\'Info</title>
-			
-			<link rel="stylesheet" type="text/css" href="styles.css"/>
-			<script language="javascript" type="text/javascript" src="jquery.js"></script>
-			<script language="javascript" type="text/javascript" src="jquery.easing.1.3.js"></script>
-			<script language="javascript" type="text/javascript" src="script.js"></script>
-			
-		</head>
-		<body>
-			<center><h1>Voici la BASE</h1></center>
-		</body>
-';
 
 
 echo "<center><h1>Voici la Base de zizi</h1></center>";
 echo 'coucou';
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-
-=======
->>>>>>> 2c6ade178e67061f7d9a6b4df6d957f588eb6078
-=======
->>>>>>> 7464923a115dfa9f09c6a36fb8dff4d638db349a
-=======
->>>>>>> febb8d5bc5fee730684c9cf200d7074ede559b79
 ?>
